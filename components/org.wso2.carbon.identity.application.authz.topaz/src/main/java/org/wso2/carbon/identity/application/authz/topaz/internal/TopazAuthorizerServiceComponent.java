6ab179d650960cf143cf04a77658f1b57ee5969c
PRO-GUNE <chathuragunasekera@gmail.com>
2024-04-30 13:17:57 +0530

    Created service components - for OSGi framework

---
+++ components/org.wso2.carbon.identity.application.authz.topaz/src/main/java/org/wso2/carbon/identity/application/authz/topaz/internal/TopazAuthorizerServiceComponent.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2024, WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.identity.application.authz.topaz.internal;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.wso2.carbon.identity.application.authz.topaz.handler.abs.AuthorizerInterface;
+import org.wso2.carbon.identity.application.authz.topaz.handler.impl.TopazAuthorizerHandler;
+
+@Component(
+        name = "identity.application.authz.topaz.component",
+        immediate = true
+)
+public class TopazAuthorizerServiceComponent {
+    private static final Log log = LogFactory.getLog(TopazAuthorizerServiceComponent.class);
+
+    @Activate
+    protected void activate(ComponentContext ctxt) {
+        try {
+            TopazAuthorizerHandler topazAuthorizerHandler = new TopazAuthorizerHandler();
+            ctxt.getBundleContext().registerService(AuthorizerInterface.class, topazAuthorizerHandler, null);
+            if (log.isDebugEnabled()) {
+                log.debug("Application Topaz authorizer handler bundle is activated");
+            }
+        } catch (Throwable throwable) {
+            log.error("Error while starting topaz authorization component", throwable);
+        }
+    }
+
+    @Deactivate
+    protected void deactivate(ComponentContext context) {
+        log.debug("Application Topaz authorizer handler bundle is deactivated.");
+    }
+}
